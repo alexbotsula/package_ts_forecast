fcc5ba5a6f701f5a714e4588a7069f8f0d253d90
alex <abotsula@gmail.com>
2020-12-24 13:26:13 +1100

    structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='ts_forecast',
+    url='https://github.com/alexbotsula/package_ts_forecast',
+    author='Alex Botsula',
+    author_email='abotsula@gmail.com',
+    packages=['ts_forecast'],
+    install_requires=['pandas', 'numpy', 'math', 'sklearn'],
+    version='0.1',
+    license='MIT',
+    description='Package includes utility classes and function for time series transformations and forecasting'
+)
