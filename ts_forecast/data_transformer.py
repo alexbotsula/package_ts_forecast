4dfd54861a213a83a7ddd34f48196178ec3d7ca4
alex <abotsula@gmail.com>
2021-01-02 07:13:51 +1100

    3D Transform added

--- ts_forecast/data_transformer.py
+++ ts_forecast/data_transformer.py
@@ -15,7 +15,7 @@ class DataTransformerBase(ABC):
             forecast_horizon (int):     defines the forecast fhorizon in minutes
             history_used (int):         defines the historic period considered for the forecast
             x_variables (list):         defines the variables used in the forecast, i.e. OHLCV
-            y_variable: (string):       name of the variable used for the dependent varianle in the forecast, i.e. 'O'
+            y_variable (string):        name of the variable used for the dependent varianle in the forecast, i.e. 'O'
             lower_threshold, upper_threshold (float): defines the event predicted by the forecast, i.e. -5% return over the forecast horizon; 
                 Used in the classification version of the forecast. Regression type of data is assumed if None
         '''
@@ -34,7 +34,12 @@ class DataTransformerBase(ABC):
         pass
 
 
+    @abstractmethod
     def _return_var_y(self, df):
+        pass
+
+
+    def _asset_return(self, df):
         ret = (df[[self._y_variable]].shift(-self._forecast_horizon) / df[[self._y_variable]] - 1.).values.flatten()
 
         if self._lower_threshold is not None and self._upper_threshold is not None:
@@ -51,8 +56,9 @@ class DataTransformerBase(ABC):
             df (DataFrame):     raw data used in the modelling
         '''
         
+        df_x = df.copy()
         # Normalise X values
-        df_x = pd.DataFrame(self._X_normalizer.transform(df[self._x_variables]), columns=self._x_variables, index=df.index)
+        df_x[self._x_variables] = pd.DataFrame(self._X_normalizer.transform(df[self._x_variables]), columns=self._x_variables, index=df.index)
 
         _X = self._lagged_vars_x(df_x)
         _Y = self._return_var_y(df)
@@ -84,6 +90,10 @@ class DataTransformer1D(DataTransformerBase):
         super().__init__(forecast_horizon, history_used, x_variables, y_variable, lower_threshold, upper_threshold)
 
 
+    def _return_var_y(self, df):
+        return self._asset_return(df)
+
+
     def _lagged_vars_x(self, df):
         x_data = pd.DataFrame(index=df.index)
         x_data[self._x_variables] = df[self._x_variables]
@@ -96,7 +106,42 @@ class DataTransformer1D(DataTransformerBase):
         return x_data.iloc[self._history_used:-self._forecast_horizon]
 
 
-        
+
+class DataTransformer3D(DataTransformerBase):
+    '''
+    Data transformer class, producing a 4D table (np array), 3D array per time point:
+        Features (OCHLV)
+        Lags (0..forecast_horizon)
+        Assets (BTC, ETH, ...)
+    '''
+
+    def __init__(self, forecast_horizon, history_used, x_variables, y_variable, x_assets, y_asset, lower_threshold=None, upper_threshold=None):
+        '''
+        Args (additional to base class):
+            x_assets (list):    list of assets used as an input
+            y_asset (string):   asset whose y_variable to be used for dependent variable 
+        '''
+        self._x_assets = x_assets
+        self._y_asset = y_asset
+        super().__init__(forecast_horizon, history_used, x_variables, y_variable, lower_threshold, upper_threshold)
+
+
+    def _return_var_y(self, df):
+            return self._asset_return(df[df.symb == self._y_asset])
+
+
+    def _lagged_vars_x(self, df):
+        # Pre-allocate an array containing the values
+        n_obs = len(df[df.symb == self._y_asset].index)
+        val_array = np.zeros((n_obs, len(self._x_variables), self._forecast_horizon+1, len(self._x_assets)))
+
+        for i_f, f in enumerate(self._x_variables):
+            for i_a, a in enumerate(self._x_assets):
+                for lag in range(self._forecast_horizon+1):
+                    val_array[:, i_f, lag, i_a] = df[df.symb == a][f].shift(lag)  
+
+        return val_array[self._history_used:-self._forecast_horizon,]  
+
 
 def transform_date_ccxt(date_col):
     '''
